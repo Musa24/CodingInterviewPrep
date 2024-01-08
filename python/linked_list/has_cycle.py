3194527cfe71484194a96c09cfa7112e6e01ec9f
musa24 <musalumu24@gmail.com>
2024-01-08 08:09:12 +0300

    has_cycle_qn

---
+++ python/linked_list/has_cycle.py
@@ -0,0 +1,19 @@
+#example 1
+#Input:  [A -> B, B -> C, C -> D, D -> E, E -> B]
+#Output: True
+
+#example 2
+#Input:  [1 -> 2, 2 -> 3, 3 -> 4, 4 -> null]
+#Output: False
+
+class Node:
+  def __init__(self,value):
+     self.value = value
+     self.next = None
+
+#Solution
+def has_cycle(head):
+   # your code
+   pass
+
+  
